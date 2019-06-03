a08c36c9a1006c58ffc04deba95d50564452f45f
SophieHYe <He_Ye_90s@hotmail.com>
2019-06-03 13:53:31 -0700

    update code and pom file

--- src/main/java/test/Run.java
+++ src/main/java/test/Run.java
@@ -1,76 +1,106 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Random;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.ml.linalg.DenseMatrix;
+import org.apache.spark.ml.linalg.Matrices;
 import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.linalg.distributed.BlockMatrix;
 import org.apache.spark.mllib.linalg.distributed.IndexedRow;
 import org.apache.spark.mllib.linalg.distributed.IndexedRowMatrix;
+import org.apache.spark.mllib.linalg.distributed.RowMatrix;
+import org.apache.spark.mllib.util.MLUtils;
+
+import scala.Tuple3;
+import scala.collection.Iterable;
+import scala.collection.immutable.List;
+
+import org.apache.spark.mllib.linalg.Matrix;
+import org.apache.spark.mllib.linalg.SingularValueDecomposition;
+import org.apache.spark.mllib.linalg.SparseMatrix;
+import org.apache.spark.mllib.linalg.Vector;
 
 public class Run {
 
-	public static void main(String[] args) {
-		SparkConf sparkConf = new SparkConf().setAppName("Mllib-test").setMaster("local");
+	public static void main(String[] args) throws IOException {
+//		SparkConf sparkConf = new SparkConf().setAppName("wasp-cloud");
+		SparkConf sparkConf = new SparkConf().setAppName("wasp-cloud").setMaster("local");
 		JavaSparkContext jpc = new JavaSparkContext(sparkConf);
-		double[][] data = new double[4][4] ;
-		data[0][0] = 0.0;
-		data[0][1] = 2.0;
-		data[0][2] = 3.0;
-		data[0][3] = 4.0;
+		Long start = System.currentTimeMillis();
+
+		double[] densefile = new Run().readfile();
+		DenseMatrix dm = (DenseMatrix) Matrices.dense(1138, 3411, densefile);
+		//create random dense matrix	
+//		Random rand = new Random(100);
+//		DenseMatrix dm = (DenseMatrix) Matrices.rand(5000,5000,rand);
 		
-		data[1][0] = 1.0;
-		data[1][1] = 3.0;
-		data[1][2] = 4.0;
-		data[1][3] = 5.0;
-		
-		data[2][0] = 2.0;
-		data[2][1] = 4.0;
-		data[2][2] = 5.0;
-		data[2][3] = 6.0;
-		
-		data[3][0] = 3.0;
-		data[3][1] = 5.0;
-		data[3][2] = 6.0;
-		data[3][3] = 7.0;
-		
-		JavaRDD<IndexedRow> rdd=jpc.parallelize(Arrays.asList(data)).map(f->{
+		JavaRDD<IndexedRow> rows = jpc.parallelize(Arrays.asList(dm.toArray())).map(f -> {
 			long key = new Double(f[0]).longValue();
-			double[] value = new double[f.length-1];
-			for(int i = 1;i<f.length;i++) {
-				value[i-1] = f[i];
+			double[] value = new double[f.length - 1];
+			for (int i = 1; i < f.length; i++) {
+				value[i - 1] = f[i];
 			}
-			return new IndexedRow(key,Vectors.dense(value));
+			return new IndexedRow(key, Vectors.dense(value));
 		});
-		BlockMatrix block = new IndexedRowMatrix(rdd.rdd()).toBlockMatrix(2, 2);
-		
-		double[][] data1 = new double[3][3] ;
-		data1[0][0] = 0.0;
-		data1[0][1] = 100.0;
-		data1[0][2] = 10.0;
-		
-		data1[1][0] = 1.0;
-		data1[1][1] = 10.0;
-		data1[1][2] = 100.0;
-		
-		data1[2][0] = 2.0;
-		data1[2][1] = 1.0;
-		data1[2][2] = 1000.0;
 
-		JavaRDD<IndexedRow>  rdd1 = jpc.parallelize(Arrays.asList(data1)).map(f->{
-			long key = new Double(f[0]).longValue();
-			double[] value = new double[f.length-1];
-			for(int i = 1;i<f.length;i++) {
-				value[i-1] = f[i];
+		IndexedRowMatrix indexrowmatrix = new IndexedRowMatrix(rows.rdd());
+
+		// method 1: SVD
+		SingularValueDecomposition svdcomput = indexrowmatrix.computeSVD(10, true, 0.1);
+		System.out.println(svdcomput.s());
+		Long end = System.currentTimeMillis();
+		Long executiontime = (end - start) / 1000;
+		System.out.println(executiontime);
+
+	}
+
+	public  double[] readfile() throws IOException {
+		
+		InputStream in = this.getClass().getResourceAsStream("/files/row1138.csv");
+		
+		System.out.println("*******Reading the dense matrix files******");
+		String lines = "";
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+		String st;
+		while ((st = br.readLine()) != null) {
+			lines += st;
+		}		
+		lines.replace("\n", ",");
+		lines.replace("\r", ",");		
+		Long l = (long) lines.length();
+		
+		System.out.println("strings~~~~~:" + l);
+		String[] values = lines.split(",");
+
+		System.out.println("The values~~~~~:" + values.length);
+		double[] darray = new double[values.length];
+
+		for (int i = 0; i < values.length; i++) {
+			if(null!=values[i]&&""!=values[i]) {
+				String[] p = values[i].split(".");
+				if(p.length==2) {
+			double dv = Double.parseDouble(values[i]);	
+				
+			darray[i] = dv;
 			}
-			return new IndexedRow(key,Vectors.dense(value));
-		});
-		BlockMatrix block1 = new IndexedRowMatrix(rdd1.rdd()).toBlockMatrix(2, 2);
-		block = block.multiply(block1);
-		System.out.println(block);
+			}
+		}
+		return darray;
+	}
 
-
-	}	
 }
